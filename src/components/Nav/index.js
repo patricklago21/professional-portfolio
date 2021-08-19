ccdce2ff5a7342f68c92e6d5a12a9fb87440fc93
Patrick Lago <Patrick@Dinas-Air.attlocal.net>
2021-08-19 14:25:03 -0700

    updated navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -12,7 +12,7 @@ function Nav(props) {
     return (
         <div className="container">
             <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <a href="https://patricklago21.github.io/professional-portfolio/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
                 <FileCodeFill className="mx-2" style={{ color: '#004e89' }}/>
                     <span className="fs-4 dark-blue" style={{ color: '#004e89' }}>Patrick Lago</span>
                 </a>
