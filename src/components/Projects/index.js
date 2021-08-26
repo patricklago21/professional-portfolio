995440c7666bb6f589a9256b0e457216c273596b
Patrick Lago <Patrick@Dinas-Air.attlocal.net>
2021-08-25 19:28:50 -0700

    added URU project

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import project1 from "../../assets/images/project1.png";
 import hausKeepr from "../../assets/images/hauskeepr.png";
 import webkitPic from "../../assets/images/webkit.png";
+import uru from "../../assets/images/uru.png";
 
 function Projects() {
     return (
@@ -26,14 +27,14 @@ function Projects() {
             <div className="container my-5">
                 <div className="row pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border">
                     <div className="col-lg-4 p-0 overflow-hidden shadow">
-                        <img src={webkitPic} className="rounded-lg-3" alt="" width="320"/>
+                        <img src={uru} className="rounded-lg-3" alt="" width="400" height='250'/>
                     </div>
                     
                     <div className="col-lg-7 offset-lg-1 p-3 p-lg-5 pt-lg-3">
-                        <h1 className="display-4 fw-bold lh-1 text-end" style={{ color: '#004e89' }}>Budget Tracker</h1>
-                        <p className="lead text-end" style={{ color: '#1a659e' }}>Utilizing WebKit, this application can save actions if a user's internet connection is interrupted or lost. Once the user regains connection,<br></br> their offline actions will persist.</p>
+                        <h1 className="display-4 fw-bold lh-1 text-end" style={{ color: '#004e89' }}>URU</h1>
+                        <p className="lead text-end" style={{ color: '#1a659e' }}>URU is an economic and social empowerment application that builds a connection between child and behavior therapist. Built alongside Luis Arnaut, Nell-E Medina, and Brad Smith.</p>
                         <div className="d-grid gap-2 d-md-flex justify-content-md-end mb-4 mb-lg-3">
-                            <a href="https://budget-tracker707.herokuapp.com/" target="_blank" rel="noopener noreferrer" type="button" className="btn btn-lg px-4 me-md-2 fw-bold" style={{ background: '#ff6b35' }}><span style={{ color: '#ffffff' }}>Check It Out</span></a>
+                            <a href="https://uru.herokuapp.com/" target="_blank" rel="noopener noreferrer" type="button" className="btn btn-lg px-4 me-md-2 fw-bold" style={{ background: '#ff6b35' }}><span style={{ color: '#ffffff' }}>Check It Out</span></a>
                         </div>
                     </div>
                     
@@ -53,6 +54,24 @@ function Projects() {
                     <div className="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow">
                         <img src={project1} className="rounded-lg-3" alt="" width="720"/>
                     </div>
+                </div>
+            </div>
+
+            {/* budget tracker */}
+            <div className="container my-5">
+                <div className="row pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border">
+                    <div className="col-lg-4 p-0 overflow-hidden shadow">
+                        <img src={webkitPic} className="rounded-lg-3" alt="" width="320"/>
+                    </div>
+                    
+                    <div className="col-lg-7 offset-lg-1 p-3 p-lg-5 pt-lg-3">
+                        <h1 className="display-4 fw-bold lh-1 text-end" style={{ color: '#004e89' }}>Budget Tracker</h1>
+                        <p className="lead text-end" style={{ color: '#1a659e' }}>Utilizing WebKit, this application can save actions if a user's internet connection is interrupted or lost. Once the user regains connection,<br></br> their offline actions will persist.</p>
+                        <div className="d-grid gap-2 d-md-flex justify-content-md-end mb-4 mb-lg-3">
+                            <a href="https://budget-tracker707.herokuapp.com/" target="_blank" rel="noopener noreferrer" type="button" className="btn btn-lg px-4 me-md-2 fw-bold" style={{ background: '#ff6b35' }}><span style={{ color: '#ffffff' }}>Check It Out</span></a>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
 
